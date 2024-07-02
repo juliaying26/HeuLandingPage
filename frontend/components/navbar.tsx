999f0f3791add136c35aafbb786f9f38089f7c18
Julia Ying <juliaying@princeton.edu>
2024-07-02 15:51:49 -0400

    added navbar with contact roles and donate

---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,34 @@
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <a href="mailto:contact@heulearning.org">
+        <button className={styles.nav_buttons_left}>Contact</button>
+      </a>
+      <a
+        href="https://www.indeed.com/cmp/Heu-Learning?from=gnav-one-host&hl=en_US&co=US&_gl=1*1nb7wkj*_gcl_aw*R0NMLjE3MTk5NDk2MDIuQ2p3S0NBand5bzYwQmhCaUVpd0FIbVZMSmNoUXViZHRDTGVtMUVrV3hhSFJOMENqSV9wbWx3cFRwOXhYMEJSdXBHRTY2RXRnYlR0WThSb0NGajBRQXZEX0J3RQ..*_gcl_dc*R0NMLjE3MTk5NDk2MDIuQ2p3S0NBand5bzYwQmhCaUVpd0FIbVZMSmNoUXViZHRDTGVtMUVrV3hhSFJOMENqSV9wbWx3cFRwOXhYMEJSdXBHRTY2RXRnYlR0WThSb0NGajBRQXZEX0J3RQ..*_gcl_au*ODY5OTE4MzAuMTcxOTkxNDI0Mw..*_ga*MjcxOTMzMzg2LjE3MTk5MTQyNDA.*_ga_5KTMMETCF4*MTcxOTk0ODQyNS40LjEuMTcxOTk0OTc3Mi41OC4wLjA.*_fplc*ZVJKQnlsb0JwT2xjSk52T3pVbWlEb285YSUyRlNHcTFmTElaaiUyQmlQajRjY1pibU0wUVlnaEtEOFV3YjhzUGNRcmlzN2Jvd3NtREMwRDFnbTlXRktXRGdkWEglMkJTT2VUUTJ2bEpNa2F1YWJTak5oWXBuSGtBOWRzell6NnF5RVdRJTNEJTNE"
+        target="_blank"
+      >
+        <button className={styles.nav_buttons}>Roles</button>
+      </a>
+      <a href="https://givebutter.com/heu" target="_blank">
+        <button className={styles.nav_buttons_right} id="right">
+          Donate
+        </button>
+      </a>
+    </div>
+
+    // <div className={styles.navbar}>
+    //   <div className={styles.nav_links}>
+    //     <a href="mailto:contact@heulearning.org">Contact</a>
+    //     <div className={styles.nav_divider}></div>
+    //     <a href="">Roles</a>
+    //     <div className={styles.nav_divider}></div>
+    //     <div className={styles.donate}>
+    //       <a href="https://givebutter.com/heu" target="_blank">Donate</a>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,34 +6,36 @@ import Body from "../../components/body";
 import Footer from "../../components/footer";
 import Hook from "../../components/hook";
 import styles from "../styles/Home.module.css";
+import Navbar from "../../components/navbar";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import 'aos/dist/aos.css';
-import AOS from 'aos';
+import "aos/dist/aos.css";
+import AOS from "aos";
 
-import "animate.css/animate.compat.css"
-import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.compat.css";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export default function Home() {
-
   return (
     <>
-        <Head>
-          <title>Heu Learning</title>
-          <meta name="description" content="Teach more English better" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/icon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
-        </Head>
-        
-        <div className={styles.background}>
-          <Header />
-          <Body />
-          <Footer />
-        </div>
+      <Head>
+        <title>Heu Learning</title>
+        <meta name="description" content="Teach more English better" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <div className={styles.background}>
+        <Navbar />
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </>
   );
 }
-
-
